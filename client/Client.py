58b77602f2050407183a1a952756f6daa928a8dc
raf181 <93051606+raf181@users.noreply.github.com>
2023-10-31 09:46:27 +0100

    Security Update API

--- client/Client.py
+++ client/Client.py
@@ -3,10 +3,11 @@ import json
 
 # Replace with the URL of your Flask API
 api_url = 'http://127.0.0.1:5000/get_code'
-# api_url = 'http://192.168.0.37:5000/get_code'
+
 # Define the user's credentials
 user = 'user2'  # Replace with the desired username
 password = 'password2'  # Replace with the user's password
+
 # Define the key you want to use
 key = 'key'  # Replace with the desired key
 
@@ -18,22 +19,23 @@ response = requests.post(api_url, json=payload, auth=(user, password))
 
 # Check the response status code
 if response.status_code == 200:
-    data = json.loads(response.text)
-    code = data.get('code')
-    if code:
-        # ==== debug ==== #
-        print(code)
-        # == end debug == #
-        # === execute === #
-        # exec(code)
-        # = end execute = #
-    else:
-        print("API response is missing code.")
+    try:
+        data = json.loads(response.text)
+        code = data.get('code')
+        if code:
+            # ==== debug ==== #
+            print(code)
+            # == end debug == #
+            # === execute === #
+            # exec(code)
+            # = end execute = #
+        else:
+            print("API response is missing code.")
+    except json.JSONDecodeError:
+        print("API response is not valid JSON.")
 elif response.status_code == 401:
     print("Wrong password or user. Please check your password and user.")
 elif response.status_code == 404:
     print("Key not found. Please check the key.")
 else:
-    error_data = json.loads(response.text)
-    error_message = error_data.get('error', 'Unknown error')
-    print(f"Error: {error_message}")
+    print(f"Error: API responded with status code {response.status_code}")
