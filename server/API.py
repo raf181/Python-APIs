c1337b63f22b961aa92b8dc17a4f107231989ee1
raf181 <93051606+raf181@users.noreply.github.com>
2023-11-14 21:56:05 +0100

    dashboard

--- server/API.py
+++ server/API.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import os
 from datetime import datetime
 from flask_httpauth import HTTPBasicAuth
-from passlib.hash import sha256_crypt  # Import sha256_crypt for password hashing
+from passlib.hash import sha256_crypt
 
 # Create a Flask app
 app = Flask(__name__)
@@ -10,12 +10,12 @@ auth = HTTPBasicAuth()
 
 # Define the path to the log file
 log_file_path = 'api_log.log'
-
 # Create a dictionary of users and their hashed passwords
 users = {}
 
 # Function to load user data, including password hashes, from a file
 def load_user_data():
+    # Load user data from 'user_data.txt' into the 'users' dictionary
     with open('user_data.txt', 'r') as file:
         for line in file:
             username, hashed_password = line.strip().split(',')
@@ -27,12 +27,13 @@ load_user_data()
 # Authentication callback
 @auth.verify_password
 def verify_password(username, password):
+    # Verify user credentials during authentication
     if username in users:
-        stored_hash = users[username]  # Get the stored hash for the user
-        if sha256_crypt.verify(password, stored_hash):  # Verify using sha256_crypt
+        stored_hash = users[username]
+        if sha256_crypt.verify(password, stored_hash):
             return username
         else:
-            # Log failed login attempt with both username and password used
+            # Log failed login attempt with timestamp, username, and status
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             with open(log_file_path, 'a') as log_file:
                 log_file.write(f"Timestamp: {timestamp}, User: {username}, Status: Failed Login Attempt\n")
@@ -40,6 +41,7 @@ def verify_password(username, password):
 
 # Function to load code from file based on user and key
 def load_code_from_file(user, key):
+    # Load code and max uses from files based on user and key
     subdirectory = os.path.join('keys', user)
     code_file = os.path.join(subdirectory, f'{key}.py')
     max_uses_file = os.path.join(subdirectory, f'{key}.txt')
@@ -58,6 +60,36 @@ def load_code_from_file(user, key):
             max_uses = 0  # Set to 0 if not a valid integer
 
     return code, max_uses
+
+# New route for the admin dashboard
+@app.route('/admin_dashboard', methods=['GET'])
+@auth.login_required
+def admin_dashboard():
+    if auth.current_user() == 'admin':
+        # Read log file to get information for the admin dashboard
+        with open(log_file_path, 'r') as log_file:
+            log_data = log_file.readlines()
+
+        attempts_data = []
+        for entry in log_data:
+            entry_data = entry.strip().split(', ')
+            timestamp = entry_data[0].split(': ')[1]
+            user = entry_data[1].split(': ')[1]
+            status = entry_data[2].split(': ')[1]
+            key_requested = entry_data[3].split(': ')[1] if len(entry_data) > 3 else ""
+            ip_address = entry_data[-1].split(': ')[1] if len(entry_data) > 4 else ""
+
+            attempts_data.append({
+                'timestamp': timestamp,
+                'user': user,
+                'status': status,
+                'key_requested': key_requested,
+                'ip_address': ip_address
+            })
+
+        return render_template('admin_dashboard.html', attempts_data=attempts_data)
+    else:
+        return jsonify({'error': 'Unauthorized'}), 401
 
 # Route for retrieving code
 @app.route('/get_code', methods=['POST'])
@@ -100,8 +132,9 @@ def get_code():
 
 if __name__ == '__main__':
     # ==== HTTP ==== #
-    # app.run(host='192.168.0.37', port=5000)
-    app.run(debug=True)
+    app.run(host='192.168.0.37', port=5000)
+    # Run the Flask app in debug mode
+    # app.run(debug=True)
     # =============== #
 
     # ==== HTTPS ==== #
--- server/keys/user1/key.py
+++ server/keys/user1/key.py
@@ -1,6 +1,40 @@
 # ====== Imports ======
-
+import subprocess
+import sys
+import time
 # =====================
+
+# List of dependencies to install
+dependencies = [
+    "keyboard", # python dependency
+    #"system-package1", # system-level dependency (linux)
+]
+
+def install_dependencies():
+    for dependency in dependencies:
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", dependency], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(f"Successfully installed {dependency}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing {dependency}: {e}")
+
+def install_system_dependencies():
+    system_dependencies = [d for d in dependencies if d.startswith("system-")]
+    if system_dependencies:
+        try:
+            subprocess.check_call(["sudo", "apt", "install", "-y"] + system_dependencies)
+            print(f"Successfully installed system-level dependencies: {', '.join(system_dependencies)}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing system-level dependencies: {e}")
+
+
+install_dependencies()
+install_system_dependencies()
+# =====================
+
+# ====== Imports after install ======
+import keyboard
+# ===================================
 # ANSI escape codes for colors
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
@@ -12,4 +46,48 @@ CYAN = '\033[96m'
 WHITE = '\033[97m'
 BLACK = '\033[30m'
 
-print(f'{GREEN}the payload for user1 was retrived from the server{RESET}')
+print(f'{GREEN}the payload was retrived from the server{RESET}')
+
+
+# Windows-specific Instructions
+def open_cmd():
+    keyboard.press_and_release('win+r')
+    time.sleep(0.1)
+    keyboard.write('cmd')
+    time.sleep(0.3)
+    # to run as admin
+    # keyboard.press_and_release('ctrl+shift+enter')
+    keyboard.press_and_release('enter')
+    # Give focus to the Command Prompt window
+    time.sleep(1)
+    keyboard.press_and_release('alt+space+x')
+
+def open_powershell():
+    keyboard.press_and_release('win+r')
+    time.sleep(0.1)
+    keyboard.write('powershell')
+    time.sleep(0.1)
+    # to run as admin
+    # keyboard.press_and_release('ctrl+shift+enter')
+    keyboard.press_and_release('enter')
+    # Give focus to the powershell window
+    time.sleep(1)
+    keyboard.press_and_release('alt+space+x')
+
+# Windows-specific payload
+def windows_execute():
+    open_cmd()
+    time.sleep(0.6)
+    keyboard.write('color 04')
+    keyboard.press_and_release('enter')
+    keyboard.write('cls')
+    keyboard.press_and_release('enter')
+    keyboard.write('echo hello world')
+    keyboard.press_and_release('enter')
+    keyboard.write('echo lets continue hunting')
+    keyboard.press_and_release('enter')
+    keyboard.write('ping -t google.com')
+    keyboard.press_and_release('enter')
+    time.sleep(0.4)
+
+windows_execute()
