1685bf00c92fb78ceb8947fa21c4f43b93f622e8
Rafa Ponce Vivancos <93051606+raf181@users.noreply.github.com>
2024-01-07 21:00:24 +0100

    Add Docker container restart option and resource path function
    Remove test.py file for simulating sensor data

--- Gui/Client.py
+++ Gui/Client.py
@@ -6,6 +6,15 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import numpy as np
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 class DataVisualizerApp:
     def __init__(self, root):
         self.root = root
